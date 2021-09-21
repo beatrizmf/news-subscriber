98956767517bd06af403506a0321c3912f91bc9e
Beatriz Felix <mbeatrizmfelix@gmail.com>
2021-09-21 10:28:48 -0300

    feat: add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { ActiveLink } from '../ActiveLink';
+
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image width="110" height="31" src="/images/logo.svg" alt="logo" />
+
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+      </div>
+    </header>
+  )
+}
