a0dc893bb9b5bd23011b12c4705e06d7b7c63810
Beatriz Felix <mbeatrizmfelix@gmail.com>
2021-09-21 09:50:43 -0300

    chore: initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>News Subscriber</title>
+      </Head>
+    </>
+  )
+}
+
+export default Home
