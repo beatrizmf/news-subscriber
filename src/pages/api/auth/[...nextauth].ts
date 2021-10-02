1e06bc0fa6110a090fca3f2da73fce2d013e8afd
Beatriz Felix <mbeatrizmfelix@gmail.com>
2021-10-01 22:44:36 -0300

    chore: remove signingKey

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
-import { fauna } from '../../../services/fauna';
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -13,9 +13,6 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  jwt: {
-    signingKey: process.env.JWT_SIGNIN_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
